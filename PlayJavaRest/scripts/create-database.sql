f1dea81499fa6b8edd8c4215ccfb6d69737590d6
Paweł Albecki <pawel.albecki@gmail.com>
2016-06-28 02:33:14 +0200

    update readme

--- PlayJavaRest/scripts/create-database.sql
+++ PlayJavaRest/scripts/create-database.sql
@@ -16,3 +16,6 @@ surname varchar(50),
 role_name varchar(10)
 PRIMARY KEY (username)
 );
+
+insert into user_ values ('admin','123','admin@admin','administrator','Serwisu','ROLE_ADMIN');
+insert into user_ values ('user','123','user@user','uzytkownik','Serwisu','ROLE_USER');
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Zadanie-Volanto
+
+* Angular2 RC2
+* Java 8
+* Play Framework 2.5.4
+* PostgreSQL 9.5
+
+Skrypt postgresa:
+```
+dbname: play
+PlayJavaRest/scripts/create-database.sql
+```
